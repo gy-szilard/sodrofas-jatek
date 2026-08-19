1ccd111ebc7c1a5b6be2a5ae265dbadbd3743389
Gyivicsán Szilárd <h463797stud.u-szeged.hu>
2026-08-19 15:35:23 +0200

    Ásó item hozzáadása

--- README.md
+++ README.md
@@ -25,6 +25,11 @@
 ## Sebzés és gyógyulás
 - A játék előrehaladtával az enemy egyre többet sebez (max 15-öt / ütés) és a szív egyre többet gyógyít (max 40-et / szív).
 
+## Ásó
+ - 50 killenként kapsz 1 ásót (Egy idejűleg max 3 darab lehet a pályán). 
+ - Az ásóval ki tudod szedni a falat (18 mp-ig eltűnik ilyenkor). 
+ - Ellenségre használva 10 mp-re lefagyasztja azt.
+
 ## Telefonról is játszható?
  - Igen, játszható, ugyanúgy böngészőben meg kell nyitni
  - Csak generálásbeli különbségek vannak (kisebb labirintus és kevesebb enemy)
--- game.js
+++ game.js
@@ -2,19 +2,15 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-// Érintőképernyős (mobil) eszköz detektálása
 const isTouchDevice = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
 
-// Mobilon 9 sor és 15 oszlop, de NAGYOBB (60px) téglákkal
-// Így a folyosók sokkal tágasabbak, és tartsuk a görgetésmentes nézetet!
 const rows = isTouchDevice ? 9 : 15;
 const cols = isTouchDevice ? 15 : 20;
 const cellSize = isTouchDevice ? 60 : 47.5;
 
-canvas.width = cols * cellSize;   // Mobilon: 900px, PC-n: 950px
-canvas.height = rows * cellSize;  // Mobilon: 540px, PC-n: 680px
+canvas.width = cols * cellSize;
+canvas.height = rows * cellSize;
 
-// Karakter és NPC fizikai hitbox mérete (mobilon 38px, a 60px-es folyosókon tökéletesen elfér)
 const CHAR_SIZE = isTouchDevice ? 38 : 32;
 const MALE_SCALE = 0.85;
 
@@ -23,24 +19,35 @@ const maleImg = new Image(); maleImg.src = "media/pictures/male.png";
 const femaleImg = new Image(); femaleImg.src = "media/pictures/female.png";
 const enemyImg = new Image(); enemyImg.src = "media/pictures/enemy.png";
 const rollingPinImg = new Image(); rollingPinImg.src = "media/pictures/rolling_pin.png";
+const shovelImg = new Image(); shovelImg.src = "media/pictures/shovel.png";
 
 // ===== Játék állapot =====
 let maze = [];
 let px = 0, py = 0, speed = isTouchDevice ? 4.5 : 5, facing = "right";
 let hp = 100, score = 0, gameOver = false;
 let attackTimer = 0, hitCooldown = 0;
+let activeAttackWeapon = "pin"; 
 let w = false, a = false, s = false, d = false;
 let female = false;
 let choosingCharacter = true;
 
+// ===== Ásó és fegyver váltás állapota =====
+let currentWeapon = "pin"; 
+let inventoryShovels = 0;
+let killsSinceLastShovel = 0;
+let droppedShovels = []; // { x, y }
+let pendingShovels = 0; 
+let removedWalls = []; // { r, c, timer }
+
 // ===== Sebzés növekedési mechanika =====
 let enemyDamage = 2;
 
-// ===== Szívek (Gyógyítás) állapot =====
+// ===== Szívek & Tárgyak mérete =====
 let hearts = []; 
 let killsSinceLastHeart = 0;
 const KILLS_REQUIRED_FOR_HEART = 7;
 const HEART_SIZE = isTouchDevice ? 30 : 25;
+const SHOVEL_ITEM_SIZE = isTouchDevice ? 60 : 70; 
 
 // ===== NPC (Ellenség) =====
 class NPC {
@@ -48,6 +55,7 @@ class NPC {
         this.x = x; this.y = y; this.size = CHAR_SIZE;
         this.dx = 0; this.dy = 0;
         this.facing = "right";
+        this.frozenTimer = 0;
         this.setRandomDirection();
     }
     setRandomDirection() {
@@ -60,6 +68,10 @@ class NPC {
         if (dir === 3) { this.dx = moveStep; this.facing = "right"; }
     }
     move() {
+        if (this.frozenTimer > 0) {
+            this.frozenTimer--;
+            return;
+        }
         if (!isColliding(this.x + this.dx, this.y + this.dy)) {
             this.x += this.dx; this.y += this.dy;
         } else this.setRandomDirection();
@@ -91,7 +103,6 @@ function placePlayer() {
     px = cellSize + offset; 
     py = cellSize + offset;
     npcs = [];
-    // Mobilon 5 ellenség a kisebb rácsszám miatt
     const npcCount = isTouchDevice ? 5 : 8;
     for (let i = 0; i < npcCount; i++) {
         let nx, ny, tries = 0;
@@ -150,26 +161,132 @@ function movePlayer() {
 
 function moveNPCs() { npcs.forEach(n => n.move()); }
 
-// ===== Támadás triggerelés (Megakadályozza a beragadást) =====
 function triggerAttack() {
-    // Csak akkor engedünk új ütést, ha az előző már majdnem lefutott (max 2 képkocka maradt)
     if (attackTimer <= 2) {
         attackTimer = 12;
+        activeAttackWeapon = currentWeapon;
+        if (currentWeapon === "shovel" && inventoryShovels > 0) {
+            useShovel();
+        }
     }
 }
 
-// ===== Támadás Hitbox =====
+function switchWeapon() {
+    if (currentWeapon === "pin") {
+        if (inventoryShovels > 0) {
+            currentWeapon = "shovel";
+        }
+    } else {
+        currentWeapon = "pin";
+    }
+}
+
+function spawnShovelOnGround() {
+    let sR, sC, sTries = 0;
+    do {
+        sR = Math.floor(Math.random() * rows);
+        sC = Math.floor(Math.random() * cols);
+        sTries++; if (sTries > 100) break;
+    } while (maze[sR][sC]);
+    
+    let sx = sC * cellSize + (cellSize - SHOVEL_ITEM_SIZE) / 2;
+    let sy = sR * cellSize + (cellSize - SHOVEL_ITEM_SIZE) / 2;
+
+    droppedShovels.push({ x: sx, y: sy });
+}
+
+function checkPendingShovels() {
+    while (pendingShovels > 0 && (inventoryShovels + droppedShovels.length) < 3) {
+        spawnShovelOnGround();
+        pendingShovels--;
+    }
+}
+
+function useShovel() {
+    if (inventoryShovels <= 0) return;
+
+    let targetR = Math.floor((py + CHAR_SIZE / 2) / cellSize);
+    let targetC = Math.floor((px + CHAR_SIZE / 2) / cellSize);
+
+    if (facing === "up") targetR--;
+    else if (facing === "down") targetR++;
+    else if (facing === "left") targetC--;
+    else if (facing === "right") targetC++;
+
+    let shovelUsed = false;
+
+    if (targetR > 0 && targetR < rows - 1 && targetC > 0 && targetC < cols - 1) {
+        if (maze[targetR][targetC]) {
+            maze[targetR][targetC] = false;
+            removedWalls.push({ r: targetR, c: targetC, timer: 1080 }); 
+            shovelUsed = true;
+        }
+    }
+
+    if (!shovelUsed) {
+        const atk = getAttackRect();
+        if (atk) {
+            npcs.forEach(npc => {
+                if (atk.x < npc.x + CHAR_SIZE &&
+                    atk.x + atk.w > npc.x &&
+                    atk.y < npc.y + CHAR_SIZE &&
+                    atk.y + atk.h > npc.y) {
+                    npc.frozenTimer = 600; 
+                    shovelUsed = true;
+                }
+            });
+        }
+    }
+
+    if (shovelUsed) {
+        inventoryShovels--;
+        if (inventoryShovels <= 0) {
+            currentWeapon = "pin";
+        }
+        checkPendingShovels();
+    }
+}
+
+function updateRemovedWalls() {
+    for (let i = removedWalls.length - 1; i >= 0; i--) {
+        let wall = removedWalls[i];
+        wall.timer--;
+
+        if (wall.timer <= 0) {
+            maze[wall.r][wall.c] = true;
+            
+            const pR1 = Math.floor(py / cellSize);
+            const pR2 = Math.floor((py + CHAR_SIZE) / cellSize);
+            const pC1 = Math.floor(px / cellSize);
+            const pC2 = Math.floor((px + CHAR_SIZE) / cellSize);
+
+            if ((pR1 <= wall.r && wall.r <= pR2) && (pC1 <= wall.c && wall.c <= pC2)) {
+                respawnPlayer();
+                hp = Math.floor(hp * 0.75);
+            }
+
+            removedWalls.splice(i, 1);
+        }
+    }
+}
+
 function getAttackRect() {
     if (attackTimer <= 0) return null;
-    const pinLength = isTouchDevice ? 56 : 48;
-    const pinThick = isTouchDevice ? 20 : 16;
-    if (facing === "right") return { x: px + CHAR_SIZE, y: py + 4, w: pinLength, h: pinThick };
-    if (facing === "left")  return { x: px - pinLength, y: py + 4, w: pinLength, h: pinThick };
-    if (facing === "up")    return { x: px + 4, y: py - pinLength - 4, w: pinThick, h: pinLength };
-    return { x: px + 4, y: py + CHAR_SIZE, w: pinThick, h: pinLength };
+    
+    const isShovelAttack = (activeAttackWeapon === "shovel");
+    const pinLength = isShovelAttack ? (isTouchDevice ? 56 : 48) : (isTouchDevice ? 68 : 58);
+    // Sodrófa vékonyítva (16 / 12)
+    const pinThick  = isShovelAttack ? (isTouchDevice ? 20 : 16) : (isTouchDevice ? 16 : 12);
+
+    if (facing === "right") return { x: px + CHAR_SIZE, y: py + (CHAR_SIZE - pinThick) / 2, w: pinLength, h: pinThick };
+    if (facing === "left")  return { x: px - pinLength, y: py + (CHAR_SIZE - pinThick) / 2, w: pinLength, h: pinThick };
+    if (facing === "up")    return { x: px + (CHAR_SIZE - pinThick) / 2, y: py - pinLength, w: pinThick, h: pinLength };
+    return { x: px + (CHAR_SIZE - pinThick) / 2, y: py + CHAR_SIZE, w: pinThick, h: pinLength };
 }
 
 function attackNPCs() {
+    if (activeAttackWeapon !== "pin") return; 
+
     const atk = getAttackRect();
     if (!atk) return;
     npcs.forEach(npc => {
@@ -190,8 +307,19 @@ function attackNPCs() {
             );
 
             npc.x = nx; npc.y = ny;
+            npc.frozenTimer = 0; 
             score++;
             enemyDamage = Math.min(15, 2 + Math.floor(score / 30));
+
+            killsSinceLastShovel++;
+            if (killsSinceLastShovel >= 1) {
+                if ((inventoryShovels + droppedShovels.length) < 3) {
+                    spawnShovelOnGround();
+                } else {
+                    pendingShovels++;
+                }
+                killsSinceLastShovel = 0;
+            }
 
             killsSinceLastHeart++;
             if (killsSinceLastHeart >= KILLS_REQUIRED_FOR_HEART) {
@@ -210,7 +338,6 @@ function attackNPCs() {
     });
 }
 
-// ===== Sebzés és Szívek =====
 function checkHits() {
     if (hitCooldown > 0) hitCooldown--;
     for (let npc of npcs) {
@@ -225,7 +352,7 @@ function checkHits() {
     if (attackTimer > 0) attackTimer--;
 }
 
-function checkHeartPickup() {
+function checkPickups() {
     for (let i = hearts.length - 1; i >= 0; i--) {
         let h = hearts[i];
         if (px + CHAR_SIZE > h.x && px < h.x + HEART_SIZE &&
@@ -235,36 +362,67 @@ function checkHeartPickup() {
             hearts.splice(i, 1);
         }
     }
+
+    for (let i = droppedShovels.length - 1; i >= 0; i--) {
+        let s = droppedShovels[i];
+        if (px + CHAR_SIZE > s.x && px < s.x + SHOVEL_ITEM_SIZE &&
+            py + CHAR_SIZE > s.y && py < s.y + SHOVEL_ITEM_SIZE) {
+            if (inventoryShovels < 3) {
+                inventoryShovels++;
+                droppedShovels.splice(i, 1);
+                checkPendingShovels();
+            }
+        }
+    }
 }
 
 // ===== Rajzoló funkciók =====
 function drawSprite(img, x, y, targetWidth, targetHeight, flipHorizontally = false, rotateAngle = 0) {
     ctx.save();
     if (!img.complete || img.naturalWidth === 0) { ctx.restore(); return; }
-    const aspectRatio = img.naturalWidth / img.naturalHeight;
-    let renderWidth = targetWidth;
-    let renderHeight = targetWidth / aspectRatio;
-    let drawY = y - (renderHeight - targetHeight);
 
-    ctx.translate(x + targetWidth / 2, drawY + renderHeight / 2);
+    const aspectRatio = img.naturalWidth / img.naturalHeight;
+    let renderW = targetWidth;
+    let renderH = targetWidth / aspectRatio;
+
+    const centerX = x + targetWidth / 2;
+    const centerY = y + targetHeight / 2;
+
+    ctx.translate(centerX, centerY);
     if (flipHorizontally) ctx.scale(-1, 1);
     if (rotateAngle !== 0) ctx.rotate(rotateAngle);
-    ctx.drawImage(img, -renderWidth / 2, -renderHeight / 2, renderWidth, renderHeight);
+
+    ctx.drawImage(img, -renderW / 2, -renderH / 2, renderW, renderH);
     ctx.restore();
 }
 
-function drawCharacter(x, y, isPlayer, femaleOption, currentFacing) {
+function drawCharacter(x, y, isPlayer, femaleOption, currentFacing, frozen = false) {
     let img = isPlayer ? (femaleOption ? femaleImg : maleImg) : enemyImg;
     let flip = (currentFacing === "left");
     let renderSize = (isPlayer && !femaleOption) ? CHAR_SIZE * MALE_SCALE : CHAR_SIZE;
     let drawOffset = (CHAR_SIZE - renderSize) / 2;
+
+    if (frozen) {
+        ctx.save();
+        ctx.filter = "brightness(0.7) hue-rotate(180deg)";
+    }
+
     drawSprite(img, x + drawOffset, y + drawOffset, renderSize, renderSize, flip);
+
+    if (frozen) {
+        ctx.restore();
+    }
 }
 
-// Eredeti sodrófa rajzolás (Változatlan)
-function drawRollingPin(atkRect, currentFacing) {
+function drawWeapon(atkRect, currentFacing) {
     ctx.save();
-    if (!rollingPinImg.complete || rollingPinImg.naturalWidth === 0) { ctx.restore(); return; }
+    const img = (activeAttackWeapon === "shovel") ? shovelImg : rollingPinImg;
+
+    if (!img.complete || img.naturalWidth === 0) { 
+        ctx.restore(); 
+        return; 
+    }
+
     const centerX = atkRect.x + atkRect.w / 2;
     const centerY = atkRect.y + atkRect.h / 2;
     ctx.translate(centerX, centerY);
@@ -274,10 +432,11 @@ function drawRollingPin(atkRect, currentFacing) {
     else if (currentFacing === "left") ctx.scale(-1, 1);
 
     if (currentFacing === "up" || currentFacing === "down") {
-        ctx.drawImage(rollingPinImg, -atkRect.h / 2, -atkRect.w / 2, atkRect.h, atkRect.w);
+        ctx.drawImage(img, -atkRect.h / 2, -atkRect.w / 2, atkRect.h, atkRect.w);
     } else {
-        ctx.drawImage(rollingPinImg, -atkRect.w / 2, -atkRect.h / 2, atkRect.w, atkRect.h);
+        ctx.drawImage(img, -atkRect.w / 2, -atkRect.h / 2, atkRect.w, atkRect.h);
     }
+    
     ctx.restore();
 }
 
@@ -291,7 +450,7 @@ function draw() {
         const y = canvas.height / 2;
         
         ctx.fillStyle = "#333";
-        ctx.font = isTouchDevice ? "bold 24px Arial" : "bold 24px Arial";
+        ctx.font = "bold 24px Arial";
         ctx.textAlign = "center";
         ctx.fillText("Válassz karaktert!", centerX, y - 60);
 
@@ -310,6 +469,16 @@ function draw() {
         }
     }
 
+    // Villogó falak
+    removedWalls.forEach(wall => {
+        if (wall.timer < 300) {
+            if (Math.floor(wall.timer / 15) % 2 === 0) {
+                ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+                ctx.fillRect(wall.c * cellSize, wall.r * cellSize, cellSize, cellSize);
+            }
+        }
+    });
+
     // Szívek
     hearts.forEach(h => {
         ctx.fillStyle = "red";
@@ -324,27 +493,36 @@ function draw() {
         ctx.fill();
     });
 
+    // Földön lévő ásók
+    droppedShovels.forEach(s => {
+        drawSprite(shovelImg, s.x, s.y, SHOVEL_ITEM_SIZE, SHOVEL_ITEM_SIZE, false, 0);
+    });
+
     // Karakterek
     drawCharacter(px, py, true, female, facing);
-    npcs.forEach(n => drawCharacter(n.x, n.y, false, false, n.facing));
+    npcs.forEach(n => drawCharacter(n.x, n.y, false, false, n.facing, n.frozenTimer > 0));
 
-    // Sodrófa
+    // Fegyver kirajzolása
     const atk = getAttackRect();
-    if (atk) drawRollingPin(atk, facing);
+    if (atk) drawWeapon(atk, facing);
 
     // HP sáv & Stats
     const barWidth = isTouchDevice ? 160 : 200;
-    const barHeight = isTouchDevice ? 20 : 20;
+    const barHeight = 20;
     ctx.fillStyle = "gray"; ctx.fillRect(10, 10, barWidth, barHeight);
     ctx.fillStyle = "limegreen"; ctx.fillRect(10, 10, Math.max(0, (barWidth / 100) * hp), barHeight);
     ctx.strokeRect(10, 10, barWidth, barHeight);
 
-    ctx.font = isTouchDevice ? "15px Arial" : "20px Arial"; 
-    ctx.textAlign = "center"; ctx.textBaseline = "middle";
-    ctx.fillStyle = "black"; ctx.fillText("HP: " + hp, 10 + barWidth / 2, 10 + barHeight / 2);
-    ctx.fillStyle = "goldenrod"; ctx.fillText("Pont: " + score, 10 + barWidth + 55, 10 + barHeight / 2);
-    ctx.font = isTouchDevice ? "13px Arial" : "14px Arial"; 
-    ctx.fillStyle = "darkred"; ctx.fillText("Dmg: " + enemyDamage, 10 + barWidth + 125, 10 + barHeight / 2);
+    ctx.font = isTouchDevice ? "14px Arial" : "18px Arial"; 
+    ctx.textAlign = "left"; ctx.textBaseline = "middle";
+    ctx.fillStyle = "black"; ctx.fillText("HP: " + hp, 15, 10 + barHeight / 2);
+    ctx.fillStyle = "goldenrod"; ctx.fillText("Pont: " + score, 10 + barWidth + 15, 10 + barHeight / 2);
+
+    // Fegyver HUD kijelzés
+    const weaponX = 10 + barWidth + (isTouchDevice ? 85 : 110);
+    const weaponText = (currentWeapon === "pin") ? "Sodrófa" : `Ásó (${inventoryShovels})`;
+    ctx.fillStyle = "cyan";
+    ctx.fillText(weaponText, weaponX, 10 + barHeight / 2);
 
     // Game Over
     if (gameOver) {
@@ -360,11 +538,12 @@ function draw() {
     }
 }
 
-// ===== Restart / Reset Mechanics =====
 function fullResetToCharacterSelect() {
     hp = 100; score = 0; enemyDamage = 2;
     facing = "right"; gameOver = false;
     hearts = []; killsSinceLastHeart = 0;
+    inventoryShovels = 0; killsSinceLastShovel = 0; droppedShovels = []; pendingShovels = 0; removedWalls = [];
+    currentWeapon = "pin"; activeAttackWeapon = "pin";
     choosingCharacter = true;
     w = false; a = false; s = false; d = false;
     attackTimer = 0;
@@ -376,6 +555,8 @@ function restartGame() {
     px = cellSize + offset; py = cellSize + offset;
     facing = "right"; gameOver = false;
     hearts = []; killsSinceLastHeart = 0;
+    inventoryShovels = 0; killsSinceLastShovel = 0; droppedShovels = []; pendingShovels = 0; removedWalls = [];
+    currentWeapon = "pin"; activeAttackWeapon = "pin";
     attackTimer = 0;
     generateMaze(); placePlayer();
 }
@@ -391,14 +572,15 @@ function update() {
         movePlayer();
         moveNPCs();
         checkHits();
-        checkHeartPickup();
+        checkPickups();
         attackNPCs();
+        updateRemovedWalls();
     }
     draw();
     requestAnimationFrame(update);
 }
 
-// ===== INPUT HANDLING (ASZTALI GÉP) =====
+// ===== INPUT HANDLING =====
 document.addEventListener("keydown", e => {
     if (isTouchDevice) return;
 
@@ -409,6 +591,7 @@ document.addEventListener("keydown", e => {
         if (e.key === "s" || e.key === "S") s = true;
         if (e.key === "a" || e.key === "A") a = true;
         if (e.key === "d" || e.key === "D") d = true;
+        if (e.key === "e" || e.key === "E") switchWeapon();
         if (e.key === " ") triggerAttack();
         if (e.key === "q" || e.key === "Q") respawnPlayer();
         if (e.key === "r" || e.key === "R") fullResetToCharacterSelect();
@@ -424,7 +607,6 @@ document.addEventListener("keyup", e => {
     if (e.key === "d" || e.key === "D") d = false;
 });
 
-// Canvas kattintás (Karakterválasztás, Érintés reakció)
 canvas.addEventListener("pointerdown", e => {
     if (isTouchDevice && !choosingCharacter && !gameOver) return;
 
@@ -446,7 +628,6 @@ canvas.addEventListener("pointerdown", e => {
     }
 });
 
-// ===== ÉRINTŐKÉPERNYŐS VEZÉRLŐK =====
 function setupTouchButton(id, onPress, onRelease) {
     const btn = document.getElementById(id);
     if (!btn) return;
@@ -458,7 +639,7 @@ function setupTouchButton(id, onPress, onRelease) {
 
     const endHandler = (e) => {
         if (e.cancelable) e.preventDefault();
-        onRelease();
+        if (onRelease) onRelease();
     };
 
     btn.addEventListener("touchstart", startHandler, { passive: false });
@@ -474,8 +655,9 @@ setupTouchButton("btn-up",    () => w = true,  () => w = false);
 setupTouchButton("btn-down",  () => s = true,  () => s = false);
 setupTouchButton("btn-left",  () => a = true,  () => a = false);
 setupTouchButton("btn-right", () => d = true,  () => d = false);
-setupTouchButton("btn-attack",() => { if(!gameOver && !choosingCharacter) triggerAttack(); }, () => {});
-setupTouchButton("btn-spawn", () => { if(!gameOver && !choosingCharacter) respawnPlayer(); }, () => {});
-setupTouchButton("btn-reset", () => { fullResetToCharacterSelect(); }, () => {});
+setupTouchButton("btn-attack",() => { if(!gameOver && !choosingCharacter) triggerAttack(); });
+setupTouchButton("btn-spawn", () => { if(!gameOver && !choosingCharacter) respawnPlayer(); });
+setupTouchButton("btn-reset", () => { fullResetToCharacterSelect(); });
+setupTouchButton("btn-weapon",() => { if(!gameOver && !choosingCharacter) switchWeapon(); });
 
 update();
